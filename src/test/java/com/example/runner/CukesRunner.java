53e1da3469d08d0048239e31d59dc962f8f1d4ad
Bato Unen <batounen@gmail.com>
2022-11-06 17:38:29 -0700

    CukesRunner created

---
+++ src/test/java/com/example/runner/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.example.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/example/step_definitions",
+        dryRun = false,
+        tags = "@us4"
+)
+
+public class CukesRunner {
+
+}
