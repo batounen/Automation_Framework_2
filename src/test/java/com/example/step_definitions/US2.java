94eebb6c38f74bb173c2b4adcacfecefe5bc57a4
Bato Unen <batounen@gmail.com>
2022-11-07 22:36:16 -0700

    US2 updated

--- src/test/java/com/example/step_definitions/US2.java
+++ src/test/java/com/example/step_definitions/US2.java
@@ -1,20 +1,19 @@
 package com.example.step_definitions;
 
 import com.example.pages.Login;
-import com.example.utils.TestBase;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class US2 extends TestBase {
+public class US2 {
+
+    private final Login login = new Login();
 
     @When("user enter invalid {string} and {string}")
     public void user_enter_invalid_and(String username, String password) {
-        login = new Login();
         login.usernamePassword(username, password);
     }
     @Then("verify {string} message should be displayed")
     public void verify_message_should_be_displayed(String expectedErrorMsg) {
-        login = new Login();
         login.errorMsgVerification(expectedErrorMsg);
     }
 
