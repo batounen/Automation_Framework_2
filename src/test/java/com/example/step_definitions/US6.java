50026f05e6719d8809379f7f1708f3d99539ff65
Bato Unen <batounen@gmail.com>
2022-11-08 11:45:57 -0700

    US6 added

---
+++ src/test/java/com/example/step_definitions/US6.java
@@ -0,0 +1,37 @@
+package com.example.step_definitions;
+
+import com.example.pages.Files;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US6 {
+
+    Files files = new Files();
+
+    @When("user choose the {string} option from action-icon")
+    public void user_choose_the_option(String command) {
+        files.commandAllFiles(command);
+    }
+
+    @Then("Verify that the file is removed from the Favorites sub-module’s table")
+    public void verify_that_the_file_is_removed_from_the_favorites_sub_module_s_table() {
+        files.clickFavorites();
+        files.verifyNoFavorites();
+    }
+
+    @When("the user clicks the add icon on the top")
+    public void the_user_clicks_the_add_icon_on_the_top() {
+
+    }
+
+    @When("users uploads file with the “upload file” option")
+    public void users_uploads_file_with_the_upload_file_option() {
+
+    }
+
+    @Then("verify the file is displayed on the page")
+    public void verify_the_file_is_displayed_on_the_page() {
+
+    }
+
+}
---
+++ src/test/resources/features/US6.feature
@@ -0,0 +1,16 @@
+@us6 @smoke
+Feature: As a user, I should be able to remove files from favorites and upload a file directly
+
+  Background:
+    Given user on the dashboard page
+    When the user clicks the "Files" module
+
+  Scenario: verify users to remove files to Favorites
+    When  user choose the "Remove from favorites" option from action-icon
+    And user click the "Favorites" sub-module on the left side
+    Then Verify that the file is removed from the Favorites sub-module’s table
+
+  Scenario: verify users to upload a file from Files
+    When the user clicks the add icon on the top
+    And users uploads file with the “upload file” option
+    Then verify the file is displayed on the page
