361e08a23c97df88af58f3ed6b139fd8c320352a
Bato Unen <batounen@gmail.com>
2022-11-07 22:36:07 -0700

    US1 updated

--- src/test/java/com/example/step_definitions/US1.java
+++ src/test/java/com/example/step_definitions/US1.java
@@ -2,26 +2,24 @@ package com.example.step_definitions;
 
 import com.example.pages.Login;
 import com.example.utils.Driver;
-import com.example.utils.TestBase;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class US1 extends TestBase {
+public class US1 {
+
+    private final Login login = new Login();
 
     @Given("user on the login page")
     public void user_on_the_login_page() {
-        login = new Login();
         login.goLoginPage();
     }
     @When("user use username {string} and passcode {string}")
     public void user_use_username_and_passcode_userpass123(String username, String password) {
-        login = new Login();
         login.usernamePassword(username, password);
     }
     @When("user click the login button")
     public void user_click_the_login_button() {
-        login = new Login();
         login.loginButtonClick();
     }
     @Then("verify the user should be at the dashboard page")
--- src/test/resources/features/US1.feature
+++ src/test/resources/features/US1.feature
@@ -1,4 +1,4 @@
-@us1
+@us1 @smoke
 Feature: As a user, I should be able to log in.
   Scenario Outline: Verify login with valid credentials
     Given user on the login page
