fd0106a63f7682b242d6c3b163a922e13e870885
batounen <batounen@gmail.com>
2022-11-12 16:54:27 -0700

    Dashboard updated

--- src/test/java/com/example/pages/Dashboard.java
+++ src/test/java/com/example/pages/Dashboard.java
@@ -1,6 +1,7 @@
 package com.example.pages;
 
 import com.example.utils.Driver;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
@@ -9,7 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Dashboard {
 
@@ -17,11 +18,28 @@ public class Dashboard {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    String searchFileName;
+
     @FindAll(@FindBy(css = "#appmenu  a"))
     private List<WebElement> menuList;
 
     @FindBy(css = ".avatardiv-shown")
     private WebElement userInfo;
+
+    @FindBy(css = "a[class='header-menu__trigger']")
+    private WebElement searchBtn;
+
+    @FindBy(css = ".icon-search")
+    private WebElement searchIcon;
+
+    @FindAll(@FindBy(css = ".panel--content li span[class='name']"))
+    private List<WebElement> allRecommendedFiles;
+
+    @FindBy(css = "input[type='search']")
+    private WebElement searchInput;
+
+    @FindAll(@FindBy(css = ".unified-search__result-content h3 strong"))
+    private List<WebElement> searchResult;
 
     public WebElement getUserInfo() {
         return userInfo;
@@ -45,6 +63,32 @@ public class Dashboard {
             }
         }
         Driver.sleep(2);
+    }
+
+    public void searchBtnClick() {
+        searchBtn.click();
+        Driver.waitUntilVisible(searchIcon);
+    }
+
+    public void searchRandomFile() {
+        List<String> filesVisibleOnDashboard = new ArrayList<>();
+        for (WebElement eachFile : allRecommendedFiles) {
+            filesVisibleOnDashboard.add(eachFile.getText());
+        }
+        int randomNum = Driver.randomNumberGenerator(0, filesVisibleOnDashboard.size());
+        searchFileName = filesVisibleOnDashboard.get(randomNum);
+        searchInput.sendKeys(searchFileName + Keys.ENTER);
+        Driver.sleep(3);
+    }
+
+    public void verifySearchResult() {
+        List<String> searchResults = new ArrayList<>();
+        for (WebElement eachResult : searchResult) {
+            searchResults.add(eachResult.getText());
+        }
+        System.out.println("Search File Name = " + searchFileName);
+        System.out.println("Search Results = " + searchResults);
+        assertTrue(searchResults.contains(searchFileName));
     }
 
 }
