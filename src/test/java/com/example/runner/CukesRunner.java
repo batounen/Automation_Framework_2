97182bb195e3eceb81a01874cf89ecfd131e2761
batounen <batounen@gmail.com>
2022-11-12 16:56:59 -0700

    project files updated

--- src/test/java/com/example/runner/CukesRunner.java
+++ src/test/java/com/example/runner/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/example/step_definitions",
         dryRun = false,
-        tags = "@us6",
+        tags = "@wip",
         publish = true
 )
 
